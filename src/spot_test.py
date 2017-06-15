78d3f0351db0f34cec4cb39c83ee8b6102e8b12a
Anže Pečar <anze@pecar.me>
2017-06-15 01:16:25 +0100

    Add test for None branch

--- src/spot_test.py
+++ src/spot_test.py
@@ -15,3 +15,10 @@ def test_to_msw_id_case_insensitive() -> None:
     res = spot.to_msw_id("carcavelos")
     assert res == 912, \
         "to_msw_id() return expected {} but returned {}".format(912, res)
+
+
+def test_to_msw_id_no_spot_found() -> None:
+    """Test if returns None when no spot found"""
+    res = spot.to_msw_id("invalid spot")
+    assert res is None, \
+        "to_msw_id() return expected {} but returned {}".format(None, res)
